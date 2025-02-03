f42d62fb9558c972ee20db2e6188a84be899b1f7
Priyanshu Arora <120247490+Priyanshu1472@users.noreply.github.com>
2025-02-03 18:13:29 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -28,10 +28,27 @@ const clinics = {
 
 // Configure CORS to allow requests from the frontend\
 
+const cors = require('cors');
+
+// CORS Configuration
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://13.127.244.127:3000"
+];
+
 app.use(cors({
-  origin: 'http://13.127.244.127:3000',
-  credentials: true
+  origin: function(origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
 }));
+
+// app.options('*', cors());  // Enable pre-flight for all routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
